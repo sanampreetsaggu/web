076482fcefe5a261573891cf57c18b5737811f16
sanampreet saggu <sanampreet.saggu@evry.com>
2017-08-08 15:26:23 +0530

    initial

---
+++ src/com/src/pkg/MyClss.java
@@ -0,0 +1,8 @@
+package com.src.pkg;
+
+public class MyClss {
+	public static void main(String[] args) {
+		Sum app =new Sum();
+		System.out.println(app.add(5, 8));
+	}
+}
---
+++ src/com/src/pkg/Sum.java
@@ -0,0 +1,8 @@
+package com.src.pkg;
+
+public class Sum {
+	public int add(int a, int b) {
+		return a + b;
+
+	}
+}
